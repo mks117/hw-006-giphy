c056b87b03a560575addc5ec96cad3c86cdcad68
Max S <mks117@gmail.com>
2017-07-31 02:11:18 -0700

    fixed everything

--- assets/app.js
+++ assets/app.js
@@ -1,161 +1,60 @@
-//
-//	Maxwell Schmelling
-//	HW #6 - GIPHY API
-//	7/20/2017
-//
-
-// GLOBAL VARIABLS
-var topics = [["animals", ["cat", "kangaroo", "octopus", "dog", "bird"]],
-			["cartoons", ["king of the hill", "rick and morty", "adventure time", "spongebob sqaurepants", "the simpsons"]], 
-			["reactions", ["sorry", "thank you", "not sure", "nope", "please"]]];
-var currentTopic;
-var currentTopicTitle;
-// END GLOBAL VARIABLES
-
-
-// MAIN
-$( document ).ready(function() {
-	// stop or start a gif
-	$(".gif").on("click", function() {
-
-		var state = $(this).attr("data-state");
-
-		if(state === "still") {
-			$(this).attr("src", $(this).attr("data-animate"));
-			$(this).attr("data-state", "animate")
-		}
-
-		else if(state === "animate") {
-			$(this).attr("src", $(this).attr("data-still"));
-			$(this).attr("data-state", "still");
-		}
-		console.log(state);
-	});
-
-	$(".topic").on("click", function() {
-		GenerateSubTopics($(this).attr("data-topic"));
-	});
+$(function(){
+	generateButtons(topics, "btn btn-primary topicButton" , "#buttons");
+	console.log("Page Loaded");
 });
-// END MAIN
 
-// FUNCTIONS
-function GenerateSubTopics(topic) {
-	// set appropriate global variables
-	if(topic === "animals") {
-		console.log("animals!")
-		currentTopicTitle = topics[0][0];
-		currentTopic = topics[0][1];
-		LoadScreen(1);
-		console.log(currentTopicTitle);
-	}
+var topics = ["nope", "sorry", "thanks"];
 
-	else if (topic === "cartoons") {
-		console.log("cartoons!")
-		currentTopicTitle = topics[1][0];
-		currentTopic = topics[1][1];
-		LoadScreen(1);
-		console.log(currentTopicTitle);
-	}
-
-	else if (topic ==="reactions") {
-		console.log("reactions!")
-		currentTopicTitle = topics[2][0];
-		currentTopic = topics[2][1];
-		LoadScreen(1);
-		console.log(currentTopicTitle);
-	}
-
-	console.log(currentTopic);
-	// generate sub topic buttons inside #sub-buttons\
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	for (var i = 0; i < currentTopic.length; i++) {
-		console.log(currentTopic[i]);
-	}
-
-}
-
-function SentenceCase (str) {
-  if ((str === null) || (str === ''))
-       return false;
-  else
-   str = str.toString();
-
- return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-
-function LoadScreen(screen) {
-	// Topics Screen
-	if(screen === 0) {
-		console.log("Loading Topics!");
-		$("#topics").css({"display": "block"})
-		$("#sub-topics").css({"display": "none"})
-
-	}
-
-	// Sub Topics Screen
-	else if (screen === 1) {
-		console.log("Loading Sub Topics!");
-		currentTopicTitle = SentenceCase(currentTopicTitle);
-		$("#span-topic").text(currentTopicTitle);
-		$("#topics").css({"display": "none"})
-		$("#sub-topics").css({"display": "block"})
+function generateButtons(topics, newClass, newDiv){
+	$(newDiv).empty();
+	for (var i = 0; i < topics.length; i++) {
+		var btn = $("<button>");
+		btn.addClass(newClass);
+		btn.attr("data-type", topics[i]);
+		btn.text(topics[i]);
+		$(newDiv).append(btn);
 	}
 }
 
-// END FUNCTIONS
+$(document).on("click", ".topicButton", function(){
+	var type = $(this).data("type");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+type+"&api_key=fd2958b66fb44fd48acaebe070ebea07&limit=10";
+	$.ajax({url:queryURL, method:"GET"})
+		.done(function(response){
+			for (var i = 0; i < response.data.length; i++) {
+				var topicDiv = $("<div class='search-item'>");
+				var rating = response.data[i].rating;
+				var pText = $("<p>").text("Rating: "+rating);
+				var animated = response.data[i].images.fixed_height.url;
+				var still = response.data[i].images.fixed_height_still.url;
+				var image = $("<img>");
+				image.attr("src", still);
+				image.attr("data-still", still)
+				image.attr("data-animated", animated);
+				image.attr("data-state", "still");
+				image.addClass("topicGif");
+				topicDiv.append(pText);
+				topicDiv.append(image);
+				$("#gifs").append(topicDiv);
+			}
+		})
+})
+
+$(document).on("click", ".topicGif", function(){
+	var state = $(this).attr("data-state");
+	if(state =="still") {
+		$(this).attr("src", $(this).data("animated"));
+		$(this).attr("data-state", "animated");
+	} else {
+		console.log("still");
+		$(this).attr("src", $(this).data("still"));
+		$(this).attr("data-state", "still");
+	}
+})
+
+$("#add-gif").on("click", function(){
+	var newTopic = $("input").eq(0).val();
+	topics.push(newTopic);
+	generateButtons(topics, "btn btn-primary topicButton" , "#buttons");
+	return false;
+})
